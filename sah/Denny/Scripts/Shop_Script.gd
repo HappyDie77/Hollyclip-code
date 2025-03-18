736ff5b22e4b2a36f1e8a9ff8f4b216c55c045c8
HappyDie77 <soodenny123@gmail.com>
2025-03-18 14:54:38 +0100

    updadae

--- sah/Denny/Scripts/Shop_Script.gd
+++ sah/Denny/Scripts/Shop_Script.gd
@@ -1,1 +1,14 @@
-extends Control
+extends CanvasLayer
+
+
+
+func _on_card_buy_1_pressed():
+	pass # Replace with function body.
+
+
+func _on_card_buy_2_pressed():
+	pass # Replace with function body.
+
+
+func _on_card_buy_3_pressed():
+	pass # Replace with function body.
